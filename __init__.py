6cc35e20fb00b5d0388f9620bc68438e96332d71
彭睿龙 <pengruilong@pengruilongdeMacBook-Air.local>
2026-03-24 01:24:19 +0800

    fix: 修复 logging 配置错误 - logger.basic_config -> logging.basicConfig

--- __init__.py
+++ __init__.py
@@ -2,5 +2,5 @@ import logging
 
 def get_logger():
     logger = logging.getLogger(__name__)
-    logger.basic_config(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO)
     return logger
